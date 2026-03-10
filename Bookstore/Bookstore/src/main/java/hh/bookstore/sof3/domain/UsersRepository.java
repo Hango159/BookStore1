aa63a40262fdebd57c558d4b2701a2b83cde5e91
Hango159 <bhu212@myy.haaga-helia.fi>
2026-03-10 11:04:46 +0200

    Delete Bookstore directory

--- Bookstore/Bookstore/src/main/java/hh/bookstore/sof3/domain/UsersRepository.java
+++
@@ -1,6 +0,0 @@
-package hh.bookstore.sof3.domain;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersRepository extends CrudRepository<AppUsers, Long> {
-    AppUsers findByUsername(String username);
-}
